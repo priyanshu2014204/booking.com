730f49495d67b953f882e515939ba48dcdb7a4c4
priyanshu2014204 <priyanshu2014204@gmail.com>
2023-04-07 22:53:21 +0530

    added product by browse component

--- client/components/propertylist/propertylist.jsx
+++ client/components/propertylist/propertylist.jsx
@@ -3,38 +3,38 @@ import "./propertylist.css"
 const Property = () => {
     return (  <div className="propertylist">
           <div className="propertylistitem">
-            <img  className="plistimg" src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018207.jpg?k=e450b3e64b76c469e6d119760463eaca26a95f7fd9187fa1422cff0a5cbd4308&o=" alt="" />
+            <img  className="plistimg" src="https://r-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" />
                 <div className="plistitles">
                     <h1>Hotels</h1>
-                    <h2>1212 hotel</h2>
+                    <h2>953,698</h2>
                 </div>
           </div>
           <div className="propertylistitem">
-            <img  className="plistimg" src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018207.jpg?k=e450b3e64b76c469e6d119760463eaca26a95f7fd9187fa1422cff0a5cbd4308&o=" alt="" />
+            <img  className="plistimg" src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/119467716.jpeg?k=63b69100225782d08fbd4d0205bf949c0be894ab946a0366edb8ad48e9c0ef46&o=" alt="" />
                 <div className="plistitles">
-                    <h1>Hotels</h1>
-                    <h2>1212 hotel</h2>
+                    <h1>Appartment</h1>
+                    <h2>1,035,129</h2>
                 </div>
           </div>
           <div className="propertylistitem">
-            <img  className="plistimg" src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018207.jpg?k=e450b3e64b76c469e6d119760463eaca26a95f7fd9187fa1422cff0a5cbd4308&o=" alt="" />
+            <img  className="plistimg" src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/45450084.jpeg?k=f8c2954e867a1dd4b479909c49528531dcfb676d8fbc0d60f51d7b51bb32d1d9&o=" alt="" />
                 <div className="plistitles">
-                    <h1>Hotels</h1>
-                    <h2>1212 hotel</h2>
+                    <h1>Resorts</h1>
+                    <h2>19,346</h2>
                 </div>
           </div>
           <div className="propertylistitem">
-            <img  className="plistimg" src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018207.jpg?k=e450b3e64b76c469e6d119760463eaca26a95f7fd9187fa1422cff0a5cbd4308&o=" alt="" />
+            <img  className="plistimg" src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/100235855.jpeg?k=61ef6692e05b5971e2e8dc75687f844e6d0ad295a9a5ace17f7c713f167e61b5&o=" alt="" />
                 <div className="plistitles">
-                    <h1>Hotels</h1>
-                    <h2>1212 hotel</h2>
+                    <h1>Villas</h1>
+                    <h2>210,312</h2>
                 </div>
           </div>
           <div className="propertylistitem">
-            <img  className="plistimg" src="https://cf.bstatic.com/xdata/images/xphoto/300x240/140018207.jpg?k=e450b3e64b76c469e6d119760463eaca26a95f7fd9187fa1422cff0a5cbd4308&o=" alt="" />
+            <img  className="plistimg" src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/52979454.jpeg?k=98337a30d1016ebce1cbebb9e24ba4585f535438bc455ff9efcaee27960ac8b2&o=" alt="" />
                 <div className="plistitles">
-                    <h1>Hotels</h1>
-                    <h2>1212 hotel</h2>
+                    <h1>Cabins</h1>
+                    <h2>213,922</h2>
                 </div>
           </div>
         </div>);
